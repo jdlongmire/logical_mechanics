52fbffeb346c82971f7e8fca68368b0360baf837
JAMES (JD) Longmire <Longmire.jd@gmail.com>
2025-08-21 16:41:14 -0500

    lean file cleanup

--- LM002_lean/lakefile.lean
+++ LM002_lean/lakefile.lean
@@ -4,16 +4,6 @@ open Lake DSL
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4" @ "v4.22.0"
 
-package logical_mechanics
-
-lean_lib LogicalMechanics
-
-@[default_target]
-lean_lib LeanProofs {
+package logical_mechanics {
   srcDir := "lean_proofs"
-}
-
-@[default_target]
-lean_exe logical_mechanics {
-  root := `Main
 }
